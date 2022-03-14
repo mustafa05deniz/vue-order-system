115d6d7b42cf526183380cb615e27439ad33a99a
mustafa.deniz <mustafa.deniz@caretta.net>
2022-03-14 10:05:37 +0300

    added firebase env

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-const db = firebase.initializeApp({
-}).firestore()
+const db = firebase.initializeApp(process.env.firebase).firestore()
 export default db;
